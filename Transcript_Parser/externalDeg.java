7bb1992010daf40ae96e403a86ef24682b50b58e
benchungus <90946677+benchungus@users.noreply.github.com>
2022-11-13 19:04:54 -0600

    added parser files
    i realized its missing like a few lines of code to be actually complete, ill fix it later

---
+++ Transcript_Parser/externalDeg.java
@@ -0,0 +1,12 @@
+public class externalDeg {
+    String school;
+    String degreeName;
+    String dateFinished;
+
+    public externalDeg(String s, String dN, String dF){
+        school = s;
+        degreeName = dN;
+        dateFinished = dF;
+    }
+    
+}
